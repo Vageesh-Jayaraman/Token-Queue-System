4d22a054a3571c3c786b7f9e3d4779d6cfd64084
Vageesh-Jayaraman <jvageesh11@outlook.com>
2025-07-30 00:20:42 +0530

    Set up consumer_notification application

---
+++ consumer-notification/src/main/java/com/example/consumer_notification/ConsumerNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumer_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerNotificationApplication.class, args);
+	}
+
+}
