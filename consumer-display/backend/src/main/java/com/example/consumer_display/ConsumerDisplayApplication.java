995f416623bf39e201d54c642ea6985e964ac454
Vageesh-Jayaraman <jvageesh11@outlook.com>
2025-08-02 11:36:45 +0530

    Set up basic Consumer Display Project

---
+++ consumer-display/backend/src/main/java/com/example/consumer_display/ConsumerDisplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.consumer_display;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerDisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerDisplayApplication.class, args);
+	}
+
+}
---
+++ consumer-display/backend/src/test/java/com/example/consumer_display/ConsumerDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consumer_display;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerDisplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
