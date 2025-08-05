04bb78cc03fad6465a8b958c942ce94a4d84ed34
Vageesh-Jayaraman <jvageesh11@outlook.com>
2025-08-05 22:35:36 +0530

    Set up basic department_system app

---
+++ department-system/src/test/java/com/example/department_system/DepartmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.department_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
